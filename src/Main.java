e6de7a6a70dbd20559c7291b22aa2c8df6b91d96
Salam4ik <andrej160195@mail.ru>
2023-01-27 15:30:44 +0300

    add min() & average()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{1, 5, 20});
+        SalesManager salesManager = new SalesManager(new int[]{1, 6, 5, 20});
         System.out.println(salesManager.max());
+        System.out.println(salesManager.average());
     }
 }
